5390132fcbdbb629ccc6f399ffbf27a724ba44a2
sergejapetenok <exorich@yandex.ru>
2020-02-27 22:29:06 +0400

    Initial commit

---
+++ arrays_names.rb
@@ -0,0 +1,9 @@
+puts "Мужчины"
+male = ["Gena", "George", "Misha"]
+puts male.to_s
+puts "Женщины"
+female = ["Katrin", "Liza", "Masha"]
+puts female.to_s
+puts "Все вместе"
+mix = male + female
+puts mix.to_s
